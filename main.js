c89c2c3dbcb2d661b0f84c2ed1412388e3b7412c
Mateo <58282781+Matius000@users.noreply.github.com>
2022-12-02 15:09:16 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,37 +1,38 @@
+// Todos los derechos reservados, Mateo Vanegas Giraldo #https://matius000.github.io/portafolio/ 
 function start() {
-    if (annyang) {
-        annyang.setLanguage("es-CO")
-        annyang.start({ autoRestart: true, continuous: true });
-        console.log("Listening...")
-        annyang.addCommands(comandos);
-        annyang.debug()
-        document.getElementById("recorrido").style.visibility = "visible";
-        document.getElementById("rogelio").style.visibility = "visible";
-        document.getElementById("pregunta1").style.visibility = "visible";
-        document.getElementById("centrociencias").style.visibility = "visible";
-        document.getElementById("pregunta2").style.visibility = "visible";
-        document.getElementById("pregunta3").style.visibility = "visible";
-        document.getElementById("pregunta4").style.visibility = "visible";
-        document.getElementById("pregunta5").style.visibility = "visible";
-        document.getElementById("btn").style.visibility = "hidden";
 
-        document.getElementById("animastop").style.visibility = "visible";
-        document.getElementById("txt").style.display = "none";
-        setTimeout(() => {
-            voz("saludo");
-        }, 1000)
-    }
+    /*  
+    if (annyang){}
+    annyang.setLanguage("es-CO")
+     annyang.start({ autoRestart: true, continuous: true });
+     console.log("Listening...")
+     annyang.addCommands(comandos);
+     annyang.debug() */
+    document.getElementById("recorrido").style.visibility = "visible";
+    document.getElementById("rogelio").style.visibility = "visible";
+    document.getElementById("pregunta1").style.visibility = "visible";
+    document.getElementById("centrociencias").style.visibility = "visible";
+    document.getElementById("pregunta2").style.visibility = "visible";
+    document.getElementById("pregunta3").style.visibility = "visible";
+    document.getElementById("pregunta4").style.visibility = "visible";
+    document.getElementById("pregunta5").style.visibility = "visible";
+    document.getElementById("btn").style.display = "none";
+
+    document.getElementById("animastop").style.visibility = "visible";
+    document.getElementById("txt").style.display = "none";
+    setTimeout(() => {
+        voz("saludo");
+    }, 1000)
 }
 
-let frances = false;
 
-annyang.addCallback('soundstart', function () {
+/* annyang.addCallback('soundstart', function () {
     console.log("sound detected")
 });
 
 annyang.addCallback('result', function () {
     console.log('sound stopped');
-});
+}); */
 
 
 function evento(cadena) {
@@ -64,7 +65,7 @@ function evento(cadena) {
 }
 
 
-const comandos = {
+/* const comandos = {
     //  COMANDOS DE VOZ
 
     "(quién te) creó": () => {
@@ -137,9 +138,11 @@ const comandos = {
         voz("distrito");
         console.log("Distrito")
     },
-}
+} */
 
+var countf = 0;
 var speaking = false;
+var who;
 function voz(texto) {
 
     const saludo = new Audio('voice/saludo.mp3');
@@ -166,6 +169,7 @@ function voz(texto) {
             speaking = true;
             switch (texto) {
                 case "saludo":
+                    who = saludo;
                     saludo.play();
                     setTimeout(() => {
                         speaking = false;
@@ -174,6 +178,7 @@ function voz(texto) {
                     break;
 
                 case "buenastardes":
+                    who = buenastardes;
                     buenastardes.play()
                     setTimeout(() => {
                         speaking = false;
@@ -182,6 +187,7 @@ function voz(texto) {
                     break;
 
                 case "buenosdias":
+                    who = buenosdias;
                     buenosdias.play()
                     setTimeout(() => {
                         speaking = false;
@@ -190,6 +196,7 @@ function voz(texto) {
                     break;
 
                 case "centrociencias":
+                    who = centrociencias;
                     centrociencias.play()
                     setTimeout(() => {
                         speaking = false;
@@ -201,6 +208,7 @@ function voz(texto) {
                     break;
 
                 case "mateovanegas":
+                    who = mateovanegas;
                     mateovanegas.play()
                     setTimeout(() => {
                         speaking = false;
@@ -209,6 +217,7 @@ function voz(texto) {
                     break;
 
                 case "minombre":
+                    who = minombre;
                     minombre.play()
                     setTimeout(() => {
                         speaking = false;
@@ -217,6 +226,7 @@ function voz(texto) {
                     break;
 
                 case "noentendi":
+                    who = noentendi;
                     noentendi.play()
                     setTimeout(() => {
                         speaking = false;
@@ -225,6 +235,7 @@ function voz(texto) {
                     break;
 
                 case "okdameunsegundo":
+                    who = okdameunsegundo;
                     okdameunsegundo.play()
                     setTimeout(() => {
                         speaking = false;
@@ -233,6 +244,7 @@ function voz(texto) {
                     break;
 
                 case "recorrido":
+                    who = recorrido;
                     recorrido.play()
                     setTimeout(() => {
                         speaking = false;
@@ -245,6 +257,7 @@ function voz(texto) {
                     break;
 
                 case "rogelio":
+                    who = rogelio;
                     rogelio.play()
                     setTimeout(() => {
                         speaking = false;
@@ -256,6 +269,7 @@ function voz(texto) {
                     break;
 
                 case "soyunasistente":
+                    who = soyunasistente;
                     soyunasistente.play()
                     setTimeout(() => {
                         speaking = false;
@@ -264,6 +278,7 @@ function voz(texto) {
                     break;
 
                 case "distrito":
+                    who = distrito;
                     distrito.play()
                     animation(distrito.duration);
                     setTimeout(() => {
@@ -278,6 +293,7 @@ function voz(texto) {
                     break;
 
                 case "programacion":
+                    who = programacion;
                     programacion.play()
                     animation(programacion.duration);
                     setTimeout(() => {
@@ -290,6 +306,7 @@ function voz(texto) {
                     break;
 
                 case "reserva":
+                    who = reserva;
                     reserva.play()
                     animation(reserva.duration);
                     setTimeout(() => {
@@ -298,28 +315,34 @@ function voz(texto) {
                     setTimeout(() => {
                         window.open("https://ccusalmona.com/reserva-de-espacios/", "_blank", "toolbar=yes,scrollbars=yes,resizable=yes,top=100,left=600,width=1300,height=1200");
                     }, 2000)
-                        question('default', reserva.duration * 1000)
+                    question('default', reserva.duration * 1000)
                     break;
 
-                    case "maker":
-                        maker.play()
-                        animation(maker.duration);
-                        setTimeout(() => {
-                            speaking = false;
-                        }, parseInt(maker.duration) * 1000);
-                        setTimeout(() => {
-                            question('maker', maker.duration * 1000)
-                        }, 5000);
-                        break;
-
-                case "finalices":
-                    finalices.play()
-                    animation(finalices.duration);
+                case "maker":
+                    who = maker;
+                    maker.play()
+                    animation(maker.duration);
                     setTimeout(() => {
                         speaking = false;
-                    }, parseInt(finalices.duration) * 1000);
+                    }, parseInt(maker.duration) * 1000);
+                    setTimeout(() => {
+                        question('maker', maker.duration * 1000)
+                    }, 5000);
+                    break;
+
+                case "finalices":
+                    who = finalices;
+                    if (countf < 1) {
+                        finalices.play()
+                        animation(finalices.duration);
+                        setTimeout(() => {
+                            speaking = false;
+                        }, parseInt(finalices.duration) * 1000);
+                        countf++;
+                    } else { }
                     break;
                 default:
+                    who = noentendi;
                     noentendi.play()
                     setTimeout(() => {
                         speaking = false;
@@ -329,7 +352,6 @@ function voz(texto) {
             }
 
         } else {
-            console.log("Ya estoy hablando")
             wait()
         }
 
@@ -358,6 +380,10 @@ function animation(durationn) {
 }
 
 function wait() {
+    who.pause();
+    speaking = false;
+    document.getElementById("animastop").style.visibility = "visible";
+    document.getElementById("animaplay").style.visibility = "hidden";
     const Toast = Swal.mixin({
         toast: true,
         position: 'top-end',
@@ -372,118 +398,120 @@ function wait() {
 
     Toast.fire({
         icon: 'warning',
-        title: 'Debes esperar que termine de hablar!'
+        title: 'Voz interrumpida, elige nuevamente tu opción.'
     })
 }
 
 
 
 function question(question, time) {
-const tass = Swal.mixin({
-    buttonsStyling: true
-})
-    switch (question) {
-        case "rogelio":
-            tass.fire({
-                title: '¿Deseas ver más información acerca de Rogelio Salmona?',
-                text: "",
-                icon: 'question',
-                showCancelButton: true,
-                confirmButtonText: 'Sí',
-                cancelButtonText: 'No',
-                reverseButtons: true
-            }).then((result) => {
-                if (result.isConfirmed) {
-                    window.open("https://ccusalmona.com/el-centro-cultural/rogelio-salmona/", "_blank", "toolbar=yes,scrollbars=yes,resizable=yes,top=100,left=600,width=1300,height=1200");
-                    setTimeout(() => {
-                        voz("finalices")
-                    }, time-6500);
-                } else if (
-                    result.dismiss === Swal.DismissReason.cancel
-                ) {
-                    // Open tab
-                }
-            })
+    const tass = Swal.mixin({
+        buttonsStyling: true
+    })
 
-            break;
-        case "centrociencias":
-            tass.fire({
-                title: '¿Deseas ver más información acerca del Centro de Ciencias?',
-                text: "",
-                icon: 'question',
-                showCancelButton: true,
-                confirmButtonText: 'Sí',
-                cancelButtonText: 'No',
-                reverseButtons: true
-            }).then((result) => {
-                if (result.isConfirmed) {
-                    window.open("https://ccusalmona.com/nosotros/", "_blank", "toolbar=yes,scrollbars=yes,resizable=yes,top=100,left=600,width=1300,height=1200");
-                    setTimeout(() => {
-                        voz("finalices")
-                    }, time-6500);
-                } else if (
-                    result.dismiss === Swal.DismissReason.cancel
-                ) {
-                    // Open tab
-                }
-            })
+    if (!speaking) {
+        console.log("No muestra porque se detuvo");
+    } else {
+        switch (question) {
 
-            break;
-        case "distrito":
-            tass.fire({
-                title: '¿Deseas ver más información acerca del Distrito Creativo?',
-                text: "",
-                icon: 'question',
-                showCancelButton: true,
-                confirmButtonText: 'Sí',
-                cancelButtonText: 'No',
-                reverseButtons: true
-            }).then((result) => {
-                if (result.isConfirmed) {
-                    window.open("https://www.ucaldas.edu.co/portal/centro-cultural-universitario-rogelio-salmona-se-consolida-como-distrito-creativo/", "_blank", "toolbar=yes,scrollbars=yes,resizable=yes,top=100,left=600,width=1300,height=1200");
-                    setTimeout(() => {
-                        voz("finalices")
-                    }, time-6500);
-                } else if (
-                    result.dismiss === Swal.DismissReason.cancel
-                ) {
-                    // Open tab
-                }
-            })
+            case "rogelio":
+                tass.fire({
+                    title: '¿Deseas ver más información acerca de Rogelio Salmona?',
+                    text: "",
+                    icon: 'question',
+                    showCancelButton: true,
+                    confirmButtonText: 'Sí',
+                    cancelButtonText: 'No',
+                    reverseButtons: true
+                }).then((result) => {
+                    if (result.isConfirmed) {
+                        window.open("https://ccusalmona.com/el-centro-cultural/rogelio-salmona/", "_blank", "toolbar=yes,scrollbars=yes,resizable=yes,top=100,left=600,width=1300,height=1200");
+                        setTimeout(() => {
+                            voz("finalices")
+                        }, time - 6500);
+                    } else if (
+                        result.dismiss === Swal.DismissReason.cancel
+                    ) {
+                        // Open tab
+                    }
+                })
 
-            break;
-        case "maker":
-            tass.fire({
-                title: '¿Deseas ver más información acerca del Maker Space',
-                text: "",
-                icon: 'question',
-                showCancelButton: true,
-                confirmButtonText: 'Sí',
-                cancelButtonText: 'No',
-                reverseButtons: true
-            }).then((result) => {
-                if (result.isConfirmed) {
-                    window.open("https://ccusalmona.com/makerspace/", "_blank", "toolbar=yes,scrollbars=yes,resizable=yes,top=100,left=600,width=1300,height=1200");
-                    setTimeout(() => {
-                        voz("finalices")
-                    }, time);
-                } else if (
-                    result.dismiss === Swal.DismissReason.cancel
-                ) {
-                    // Open tab
-                }
-            })
+                break;
+            case "centrociencias":
+                tass.fire({
+                    title: '¿Deseas ver más información acerca del Centro de Ciencias?',
+                    text: "",
+                    icon: 'question',
+                    showCancelButton: true,
+                    confirmButtonText: 'Sí',
+                    cancelButtonText: 'No',
+                    reverseButtons: true
+                }).then((result) => {
+                    if (result.isConfirmed) {
+                        window.open("https://ccusalmona.com/nosotros/", "_blank", "toolbar=yes,scrollbars=yes,resizable=yes,top=100,left=600,width=1300,height=1200");
+                        setTimeout(() => {
+                            voz("finalices")
+                        }, time - 6500);
+                    } else if (
+                        result.dismiss === Swal.DismissReason.cancel
+                    ) {
+                        // Open tab
+                    }
+                })
 
-        case "default":
-            setTimeout(() => {
-                voz("finalices")
-            }, time);
-            break;
-        default:
-            break;
+                break;
+            case "distrito":
+                tass.fire({
+                    title: '¿Deseas ver más información acerca del Distrito Creativo?',
+                    text: "",
+                    icon: 'question',
+                    showCancelButton: true,
+                    confirmButtonText: 'Sí',
+                    cancelButtonText: 'No',
+                    reverseButtons: true
+                }).then((result) => {
+                    if (result.isConfirmed) {
+                        window.open("https://www.ucaldas.edu.co/portal/centro-cultural-universitario-rogelio-salmona-se-consolida-como-distrito-creativo/", "_blank", "toolbar=yes,scrollbars=yes,resizable=yes,top=100,left=600,width=1300,height=1200");
+                        setTimeout(() => {
+                            voz("finalices")
+                        }, time - 6500);
+                    } else if (
+                        result.dismiss === Swal.DismissReason.cancel
+                    ) {
+                        // Open tab
+                    }
+                })
+
+                break;
+            case "maker":
+                tass.fire({
+                    title: '¿Deseas ver más información acerca del Maker Space',
+                    text: "",
+                    icon: 'question',
+                    showCancelButton: true,
+                    confirmButtonText: 'Sí',
+                    cancelButtonText: 'No',
+                    reverseButtons: true
+                }).then((result) => {
+                    if (result.isConfirmed) {
+                        window.open("https://ccusalmona.com/makerspace/", "_blank", "toolbar=yes,scrollbars=yes,resizable=yes,top=100,left=600,width=1300,height=1200");
+                        setTimeout(() => {
+                            voz("finalices")
+                        }, time);
+                    } else if (
+                        result.dismiss === Swal.DismissReason.cancel
+                    ) {
+                        // Open tab
+                    }
+                })
+
+            case "default":
+                setTimeout(() => {
+                    voz("finalices")
+                }, time);
+                break;
+            default:
+                break;
+        }
     }
-
-
-
-
 }
